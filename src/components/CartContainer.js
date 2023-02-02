ab57e25d77bcdbd57fcc6f6e916754c5f1930583
mika1407 <koneautomaatio@hotmail.com>
2023-02-02 18:53:00 +0200

    Clear Cart

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,9 +1,10 @@
 import CartItem from './CartItem'
-import { useSelector } from 'react-redux'
-import cartItems from '../cartItems'
+import { useDispatch, useSelector } from 'react-redux'
+import { clearCart } from '../features/cart/cartSlice'
 
 const CartContainer = () => {
-    const {CartItems,total,amount} = useSelector((store) => store.cart)
+    const dispatch = useDispatch();
+    const {cartItems,total,amount} = useSelector((store) => store.cart)
 
   if(amount < 1){
       return <section className='cart'>
@@ -31,7 +32,10 @@ const CartContainer = () => {
                     total <span>${total}</span>
                 </h4>
             </div>
-            <button className='btn clear-btn'>clear cart</button>
+            <button 
+                className='btn clear-btn'
+                onClick={() => dispatch(clearCart())}
+                >clear cart</button>
         </footer>
     </section>
   );
